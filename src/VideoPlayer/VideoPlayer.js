b3a6454b590484252dde62bbb954378e18392f39
Tim Mickle <TimothyRMickle@gmail.com>
2020-07-16 15:27:39 -0500

    Removed unneeded comments. Removed unneeded comments from the VideoPlayer file. The comments were used for troubleshooting purpose only.

--- src/VideoPlayer/VideoPlayer.js
+++ src/VideoPlayer/VideoPlayer.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Player } from 'video-react';
-// // import thing from '../Assets/Videos/sketches/2018-10-02 05-21-07.mov'
-// import sketches from '../Projects/sketchData'
 
 export default (props) => {
-    // console.log(sketches)
+    
   return (
     <Player  playsInline> 
      
